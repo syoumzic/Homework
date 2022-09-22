78ac64be4e3285b8546a85612695b0d2845ceb9e
syoumzic <90949957+syoumzic@users.noreply.github.com>
2022-09-22 21:15:56 +0500

    Update main.js

--- Алгоритм Сжатия RLE/main.js
+++ Алгоритм Сжатия RLE/main.js
@@ -7,6 +7,23 @@ function mult(char, times) {
     return s
 }
 
+function decode(code) {
+    decode = ''
+    i = 0
+
+    while (i < code.length) {
+        if (code.charAt(i) == '#') {
+            decode += mult(code.charAt(i + 2), code.charAt(i + 1).charCodeAt(0))
+            i += 3
+        } else {
+            decode += code.charAt(i)
+            i += 1
+        }
+    }
+
+    return decode
+}
+
 function code(decode) {
     code = ''
     cnt = 1
@@ -22,7 +39,6 @@ function code(decode) {
 
             cnt = 1
         }
-
         else {
             code += mult(decode.charAt(i), cnt)
             cnt = 1
@@ -50,20 +66,10 @@ function decode(code) {
 }
 
 let fs = require('fs')
-decodeStr = fs.readFileSync('input.txt').toString();
-//decodeStr = fs.readFileSync("C:\\Users\\rofet\\Изображения\\macos-sierra-apple-mac-os-x.jpg").toString()
-console.log("input: ", decodeStr, '\n')
+let input = fs.readFileSync('input.txt').toString();
 
-codeStr = code(decodeStr)
-console.log("code:", codeStr, '\n')
+let codeStr = code(input)
+fs.writeFileSync('code.txt', codeStr);
 
-codeWithDecodeStr = decode(codeStr)
-console.log("decode:", codeWithDecodeStr, '\n')
-
-if(codeWithDecodeStr != decodeStr)
-    throw new Error("переделывай!")
-else {
-    fs.writeFileSync('code.txt', codeStr);
-    fs.writeFileSync('output.txt', codeWithDecodeStr)
-    console.log("file saved")
-}
+let decodeStr = decode(codeStr)
+fs.writeFileSync('output.txt', decodeStr)
