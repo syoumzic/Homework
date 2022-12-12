34dd1f90d7ced27622ba4ba172129353c543a4f3
syoumzic <90949957+syoumzic@users.noreply.github.com>
2022-12-12 20:03:18 +0500

    Create main.js

---
+++ Алгоритм Бойера-Мура/main.js
@@ -0,0 +1,77 @@
+function getShift(template){
+    shifts = new Array(template.length);
+   
+    pi = new Array(template.length);  //перевëрнутая префикс функции
+    pi[0] = 0;
+    
+    last = template.length - 1;
+    for(let i = 1; i < template.length; ++i){
+        j = pi[i - 1];
+       
+        while(j > 0 && template[last - i] != template[last - j])
+            j = pi[j - 1];
+          
+        if(template[last - i] == template[last - j])
+            ++j;
+        pi[i] = j;
+    }
+   
+    rpr = new Array(template.length);
+    for(let i = pi[last]; i < template.length; ++i)
+        rpr[i] = pi[last] - i;
+
+    for(let i = 0; i < template.length - 2; ++i){
+        if(template[i - pi[last - i]] != template[last - pi[last - i]]) 
+            rpr[pi[last - i]] = i;
+    }
+   
+    shifts = new Array(template.length);
+    for(let l = 0; l < template.length; ++l){
+        shifts[l] = template.length - l - rpr[l];
+    }
+   
+    return shifts;
+}
+
+function getSingle(template){
+    single = [];
+    last = template.length - 1
+    
+    for(let i = 0; i < template.length; ++i){
+        single[template[i]] = last - i;
+    }
+    return single;
+}
+
+text = "карл у клара украл караллы, клара у карл украла кларнет";
+template = "клара";
+shifts = getShift(template);
+single = getSingle(template);
+/*fs = require("fs");
+text = fs.readFileSync("");*/
+
+i = 0;
+last = template.length - 1;
+M = [];
+
+while(i < text.length - last){
+    for(l=0; l<template.length; ++l)
+        if(template[last - l] != text[i + last - l])
+           break;
+    
+    if(l == 0){
+        if(text[i + last] in single)
+            i += single[text[i + last]];
+        else
+            i += template.length;
+    }
+    else if(l == template.length){
+        M.push(i);
+        i += template.length;
+    }
+    else{
+        i += shifts[l]; 
+    }
+}
+
+console.log("Вхождения:", M);
